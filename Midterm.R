4d6a19a13d504c6b90809e89f841f91519f3c669
Julia Byrnes <jtb224@lehigh.edu>
2022-02-26 14:45:30 -0500

    Midterm.R

---
+++ Midterm.R
@@ -0,0 +1,29 @@
+# Mid Term Exam - Applied Question 
+# By Julia Byrnes 
+# BIS224-011
+
+# Clear out Console and Environment
+rm(list=ls(all=TRUE))
+cat("\014")
+
+# Should already have tidyverse installed. Load it into memory
+library(tidyverse)
+
+# Read in file AMD.csv
+temp <- getwd()
+
+AMD <- read_csv("AMD.csv")
+view(AMD)
+
+setwd(temp)
+
+# Plot the data 
+p <- ggplot(data = AMD,
+            mapping = aes(x = Date,
+                          y = Close))
+
+p + geom_point() +
+  labs(x = "Date", y = "Adjusted Closing Price",
+       title = "AMD Adjusted Closing Prices 2/24/2021 to 2/24/2022")
+
+
